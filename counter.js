25c1c0ff6a367c3041752d37588e74159d14c850
denisblaga <denis.blaga@thethunderteam.com>
2026-07-29 12:35:55 +0300

    chore: add counter condition

--- counter.js
+++ counter.js
@@ -1,5 +1,8 @@
 let counter = 0;
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 100; i++) {
+    if (counter == 50) {
+        counter -= 20;
+    }
     counter++;
     console.log(counter);
 }
